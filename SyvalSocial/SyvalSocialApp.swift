a4d31cb69077cc1557faaad9dfc605bb81945859
Zixi Yang <zyang792@wisc.edu>
2025-05-29 15:24:44 -0500

    Initial Commit

---
+++ SyvalSocial/SyvalSocialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SyvalSocialApp.swift
+//  SyvalSocial
+//
+//  Created by yousanflics on 5/29/25.
+//
+
+import SwiftUI
+
+@main
+struct SyvalSocialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SyvalSocialTests/SyvalSocialTests.swift
@@ -0,0 +1,17 @@
+//
+//  SyvalSocialTests.swift
+//  SyvalSocialTests
+//
+//  Created by yousanflics on 5/29/25.
+//
+
+import Testing
+@testable import SyvalSocial
+
+struct SyvalSocialTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
